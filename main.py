5dbe772b09d2e2592fbf45fc3984c190cafee9f4
sutirath <39222262+sutirath@users.noreply.github.com>
2020-08-02 17:11:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
-@app.post("/")
-def home():
-    return {"message":"Hello TutLinks.com"}
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
