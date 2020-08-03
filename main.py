e7829b361a7fbf94ed25dd760515df483585c305
sutirath <39222262+sutirath@users.noreply.github.com>
2020-08-03 15:20:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
 
 
-@app.route('/files/', methods=['GET', 'POST'])
-def upload_file():
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file',
-                                    filename=filename))
